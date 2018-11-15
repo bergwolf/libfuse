b78ee78e8dc92caad4ea836677d35fb30f776c61
Miklos Szeredi <mszeredi@redhat.com>
2018-11-15 15:29:51 +0100

    passthrough_ll: use uthash
    Improve performance of inode lookup by using a hash table.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- example/passthrough_ll.c
+++ example/passthrough_ll.c
@@ -62,6 +62,7 @@
 #include <sys/un.h>
 
 #include "ireg.h"
+#include "uthash.h"
 
 /* We are re-using pointers to our `struct lo_inode` and `struct
    lo_dirp` elements as inodes. This means that we must be able to
@@ -76,13 +77,16 @@ struct _uintptr_to_must_hold_fuse_ino_t_dummy_struct \
 			((sizeof(fuse_ino_t) >= sizeof(uintptr_t)) ? 1 : -1); };
 #endif
 
-struct lo_inode {
-	struct lo_inode *next; /* protected by lo->mutex */
-	struct lo_inode *prev; /* protected by lo->mutex */
-	int fd;
-	bool is_symlink;
+struct lo_key {
 	ino_t ino;
 	dev_t dev;
+};
+
+struct lo_inode {
+	UT_hash_handle hh;  /* protected by lo->mutex */
+	int fd;
+	bool is_symlink;
+	struct lo_key key;
 	uint64_t refcount; /* protected by lo->mutex */
 	uint64_t version_offset;
 	uint64_t ireg_refid;
@@ -113,7 +117,8 @@ struct lo_data {
 	int timeout_set;
 	int readdirplus_set;
 	int readdirplus_clear;
-	struct lo_inode root; /* protected by lo->mutex */
+	struct lo_inode root;
+	struct lo_inode *inodes; /* protected by lo->mutex */
 	int ireg_sock;
 	int64_t *version_table;
 	uint64_t version_table_size;
@@ -308,7 +313,7 @@ retry:
 			warn("lo_parent_and_name: failed to stat last");
 		goto fail_unref;
 	}
-	if (stat.st_dev != inode->dev || stat.st_ino != inode->ino) {
+	if (stat.st_dev != inode->key.dev || stat.st_ino != inode->key.ino) {
 		if (!retries)
 			warnx("lo_parent_and_name: filed to match last");
 		goto fail_unref;
@@ -438,19 +443,20 @@ out_err:
 static struct lo_inode *lo_find(struct lo_data *lo, struct stat *st)
 {
 	struct lo_inode *p;
-	struct lo_inode *ret = NULL;
+	struct lo_key key = {
+		.ino = st->st_ino,
+		.dev = st->st_dev,
+	};
 
 	pthread_mutex_lock(&lo->mutex);
-	for (p = lo->root.next; p != &lo->root; p = p->next) {
-		if (p->ino == st->st_ino && p->dev == st->st_dev) {
-			assert(p->refcount > 0);
-			ret = p;
-			ret->refcount++;
-			break;
-		}
+	HASH_FIND(hh, lo->inodes, &key, sizeof(key), p);
+	if (p) {
+		assert(p->refcount > 0);
+		p->refcount++;
 	}
 	pthread_mutex_unlock(&lo->mutex);
-	return ret;
+
+	return p;
 }
 
 struct msgreply {
@@ -468,8 +474,8 @@ static void get_shared(struct lo_data *lo, struct lo_inode *inode)
 		.op = IREG_GET,
 		.handle = (uintptr_t) &rep,
 		.get = {
-			.ino = inode->ino,
-			.dev = inode->dev,
+			.ino = inode->key.ino,
+			.dev = inode->key.dev,
 		},
 	};
 
@@ -540,8 +546,6 @@ static int lo_do_lookup(fuse_req_t req, fuse_ino_t parent, const char *name,
 		close(newfd);
 		newfd = -1;
 	} else {
-		struct lo_inode *prev, *next;
-
 		saverr = ENOMEM;
 		inode = calloc(1, sizeof(struct lo_inode));
 		if (!inode)
@@ -551,18 +555,13 @@ static int lo_do_lookup(fuse_req_t req, fuse_ino_t parent, const char *name,
 		inode->refcount = 1;
 		inode->fd = newfd;
 		newfd = -1;
-		inode->ino = e->attr.st_ino;
-		inode->dev = e->attr.st_dev;
+		inode->key.ino = e->attr.st_ino;
+		inode->key.dev = e->attr.st_dev;
 
 		get_shared(lo, inode);
 
 		pthread_mutex_lock(&lo->mutex);
-		prev = &lo->root;
-		next = prev->next;
-		next->prev = inode;
-		inode->next = next;
-		inode->prev = prev;
-		prev->next = inode;
+		HASH_ADD(hh, lo->inodes, key, sizeof(struct lo_key), inode);
 		pthread_mutex_unlock(&lo->mutex);
 	}
 
@@ -908,13 +907,7 @@ static void unref_inode(struct lo_data *lo, struct lo_inode *inode, uint64_t n)
 	assert(inode->refcount >= n);
 	inode->refcount -= n;
 	if (!inode->refcount) {
-		struct lo_inode *prev, *next;
-
-		prev = inode->prev;
-		next = inode->next;
-		next->prev = prev;
-		prev->next = next;
-
+		HASH_DEL(lo->inodes, inode);
 		pthread_mutex_unlock(&lo->mutex);
 		close(inode->fd);
 		put_shared(lo, inode);
@@ -1786,8 +1779,8 @@ static void setup_root(struct lo_data *lo, struct lo_inode *root)
 		err(1, "fstatat(%s)", lo->source);
 
 	root->fd = fd;
-	root->ino = stat.st_ino;
-	root->dev = stat.st_dev;
+	root->key.ino = stat.st_ino;
+	root->key.dev = stat.st_dev;
 	root->refcount = 2;
 }
 
@@ -1804,7 +1797,7 @@ int main(int argc, char *argv[])
 	umask(0);
 
 	pthread_mutex_init(&lo.mutex, NULL);
-	lo.root.next = lo.root.prev = &lo.root;
+	lo.inodes = NULL;
 	lo.root.fd = -1;
 	lo.cache = CACHE_AUTO;
 
